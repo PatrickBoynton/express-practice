4d7c8dfdbf4acdf9323085b55e038384ab6fd6b4
PatrickBoynton <jpboynton3@gmail.com>
2021-09-29 13:04:24 -0400

    Created the route for the edit form.

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/admin');
 
-router.get('/add-product', adminController.getAddProductPage);
+router.get('/add-product', adminController.getAddProduct);
 
 router.get('/product', adminController.getProducts);
 
 router.post('/product', adminController.addProducts);
 
+router.get('/edit-product/:id', adminController.getEditProduct);
 
 exports.routes = router;
